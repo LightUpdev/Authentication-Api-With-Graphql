ebceea7441f1cc53606f071fb0bdcfea06ea45db
light_dev <dgr8steve95@gmail.com>
2023-11-22 19:05:42 +0100

    Created routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Form from "./components/Form";
+import WelcomePage from "./pages/WelcomePage";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  
   return (
     <div className="App">
-      <Form />
+      <Routes>
+        <Route exact={true} path="/" element={<Form />} />
+
+        <Route path="/welcome" element={<WelcomePage />} />
+      </Routes>
     </div>
   );
 }
