6c15024c47e947caa0f39ed9b138a2011ca052a4
light_dev <dgr8steve95@gmail.com>
2023-11-26 12:26:13 +0100

    added functionality for protected routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,25 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import Form from "./components/Form";
 import WelcomePage from "./pages/WelcomePage";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
 
 function App() {
-  
+  const data = localStorage.getItem("User");
+  const user = JSON.parse(data);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    } 
+  }, [user, navigate]);
+
   return (
     <div className="App">
       <Routes>
         <Route exact={true} path="/" element={<Form />} />
-
         <Route path="/welcome" element={<WelcomePage />} />
       </Routes>
     </div>
