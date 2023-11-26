144e8ffdba5ff79b1e27d6e053783e0ac0f07147
light_dev <dgr8steve95@gmail.com>
2023-11-26 20:41:45 +0100

    updated uri from localhost to hosted backend server

--- frontend/src/apolloClient.js
+++ frontend/src/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:5100/auth",
+  uri: "https://light-dev-auth-backend.onrender.com/auth",
   cache: new InMemoryCache(),
 });
 
