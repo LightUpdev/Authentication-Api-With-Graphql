1a4fe1e5a760482e625af0ed9d0b86be08464ac0
light_dev <dgr8steve95@gmail.com>
2023-11-29 23:13:41 +0100

    updated from just name to firstName and lastName

--- backend/Model/index.js
+++ backend/Model/index.js
@@ -1,11 +1,12 @@
 import { Schema, model } from "mongoose";
 
 const UserSchema = new Schema({
-  name: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true, minLength: 6 },
   username: { type: String, required: true },
-  phoneNumber: { type: String, required: true },
+  phoneNumber: { type: String},
 });
 
 export const User = model("User", UserSchema);
