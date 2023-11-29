f0756fce87a0c0d795811ce59cb9756b47591819
light_dev <dgr8steve95@gmail.com>
2023-11-29 23:12:11 +0100

    updated from just name to firstName and lastName

--- backend/schema/index.js
+++ backend/schema/index.js
@@ -9,10 +9,11 @@ export const userType = new GraphQLObjectType({
   name: "userType",
   fields: () => ({
     id: { type: GraphQLNonNull(GraphQLID) },
-    name: { type: GraphQLNonNull(GraphQLString) },
+    firstName: { type: GraphQLNonNull(GraphQLString) },
+    lastName: { type: GraphQLNonNull(GraphQLString) },
     email: { type: GraphQLNonNull(GraphQLString) },
     password: { type: GraphQLNonNull(GraphQLString) },
     username: { type: GraphQLNonNull(GraphQLString) },
-    phoneNumber: { type: GraphQLNonNull(GraphQLString) },
+    phoneNumber: { type: GraphQLString },
   }),
 });
