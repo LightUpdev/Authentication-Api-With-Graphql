c95c336b2af75488c7e62c5a5b915033bce7f561
light_dev <dgr8steve95@gmail.com>
2023-12-01 09:54:53 +0100

    updated style and name

--- frontend/src/pages/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage.jsx
@@ -12,16 +12,18 @@ const WelcomePage = () => {
   console.log(user);
   return (
     <div className="welcome-page">
-      <button className="log-out" onClick={logOut}>
-        Log out
-      </button>
+      <div className="welcome-page-header">
+        <h3>{user?.firstName.toUpperCase()}</h3>
+        <button className="log-out" onClick={logOut}>
+          Log out
+        </button>
+      </div>
 
       <h1>About The Project</h1>
       <p>
-        You are welcome on board {user?.firstName.toLowerCase()}, this is an
-        application to show <mark>Lenity Stephen</mark> skill on building{" "}
-        <b>sign up and login authentication</b> using both{" "}
-        <b>front end and backend languages.</b>
+        This is an application showcasing Lenity Stephen skill on building
+        <b>sign up and login authentication</b> using both
+        <b> front end and backend languages.</b>
       </p>
       <p> Below are the languages and libraries use for this project:</p>
       <div className="skill-list">
