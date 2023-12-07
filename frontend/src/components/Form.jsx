c1eecb7515b3511ae7a34321524e1c7fb1eea385
light_dev <dgr8steve95@gmail.com>
2023-12-07 03:38:21 +0100

    Regex for user Xperience

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Label from "./Label";
 import Input from "./Input";
+import { BsCheckCircle } from "react-icons/bs";
 import Button from "./Button";
 import { useMutation } from "@apollo/client";
 import { LOGIN, SIGN_UP } from "../graphql/mutations";
 import { useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
 
 const Form = () => {
   const [firstName, setFirstName] = useState("");
@@ -15,7 +15,55 @@ const Form = () => {
   const [username, setUsername] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [toggleLogin, setToggleLogin] = useState(true);
+  const [isErrMsg, setIsErrMsg] = useState(false);
+  const [error, setError] = useState("");
+  const [passwordLength, setPasswordLength] = useState(false);
+  const [hasSpecialChar, setHasSpecialChar] = useState(false);
+  const [hasNumber, setHasNumber] = useState(false);
+  const [hasUppercase, setHasUppercase] = useState(false);
 
+  // CHECK IF PASSWORD HAVE SPECIAL CHARACTER
+  useEffect(() => {
+    const specialCharacters = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    if (!specialCharacters.test(password)) {
+      setHasSpecialChar(false);
+    } else {
+      setHasSpecialChar(true);
+    }
+  }, [password]);
+
+  // CHECK LENGTH OF PASSWORD
+  useEffect(() => {
+    if (password.length >= 8) {
+      setPasswordLength(true);
+    } else {
+      setPasswordLength(false);
+    }
+  }, [password]);
+
+  // CHECK IF PASSWORD HAVE NUMBER
+  useEffect(() => {
+    var numbers = /[0-9]/;
+
+    if (numbers.test(password)) {
+      setHasNumber(true);
+    } else {
+      setHasNumber(false);
+    }
+  }, [password]);
+
+  // CHECK IF PASSWORD HAVE UPPERCASE
+  useEffect(() => {
+    var uppercase = /[A-Z]/;
+
+    if (uppercase.test(password)) {
+      setHasUppercase(true);
+    } else {
+      setHasUppercase(false);
+    }
+  }, [password]);
+
+  // Form inputs
   const inputFormData = [
     {
       id: 1,
@@ -102,7 +150,7 @@ const Form = () => {
 
   const signUpFunc = async (e) => {
     e.preventDefault();
-    if ((firstName, lastName, email, password, username, phoneNumber)) {
+    if (firstName && lastName && email && password && username && phoneNumber) {
       try {
         const res = await signUp({
           variables: {
@@ -125,31 +173,55 @@ const Form = () => {
           setToggleLogin(!toggleLogin);
         }
       } catch (err) {
-        toast.error(err.message);
+        setIsErrMsg(true);
+        setError(err.message);
+        setTimeout(() => {
+          setIsErrMsg(false);
+          setError("");
+        }, 8000);
       }
+    } else {
+      setIsErrMsg(true);
+      setError("Fields must be filled");
+      setTimeout(() => {
+        setIsErrMsg(false);
+        setError("");
+      }, 3000);
     }
   };
 
   const loginFunc = async (e) => {
     e.preventDefault();
-    try {
-      const res = await login({
-        variables: { email, password },
-      });
+    if (password && email) {
+      try {
+        const res = await login({
+          variables: { email, password },
+        });
 
-      if (res?.data) {
-        toast.success("You are successfully logged in");
-        localStorage.setItem("User", JSON.stringify(res?.data?.login));
-        navigate("/welcome");
+        if (res?.data) {
+          localStorage.setItem("User", JSON.stringify(res?.data?.login));
+          navigate("/welcome");
+        }
+      } catch (err) {
+        setIsErrMsg(true);
+        setError(err.message);
+        setTimeout(() => {
+          setIsErrMsg(false);
+          setError("");
+        }, 3000);
       }
-    } catch (err) {
-      toast.error(err.message);
+    } else {
+      setIsErrMsg(true);
+      setError("Fields must be filled");
+      setTimeout(() => {
+        setIsErrMsg(false);
+        setError("");
+      }, 3000);
     }
   };
 
   return (
     <>
-      <ToastContainer />
       <div className="form-container">
         <div className="form-div">
           <div className="form-wrapper">
@@ -172,8 +244,13 @@ const Form = () => {
                     } = formData;
                     return (
                       <div className="input-field" key={id}>
-                        <Label labelName={labelName} label={label} />
+                        <Label
+                          htmlFor={inputName}
+                          labelName={labelName}
+                          label={label}
+                        />
                         <Input
+                          id={inputName}
                           name={inputName}
                           inputType={inputType}
                           required={required}
@@ -218,25 +295,97 @@ const Form = () => {
                 onClick={toggleLogin ? signUpFunc : loginFunc}
               />
             </form>
+            <div className="err">
+              <small>{isErrMsg && error}</small>
+            </div>
             <div className="switch-auth">
               {toggleLogin ? (
                 <small>
                   Already have an account ?
-                  <span onClick={() => setToggleLogin(false)}>
+                  <span
+                    onClick={() => {
+                      setToggleLogin(false);
+                      setEmail("");
+                      setPassword("");
+                    }}
+                  >
                     &nbsp; Login
                   </span>
                 </small>
               ) : (
                 <small>
                   Don't have an account ?
-                  <span onClick={() => setToggleLogin(true)}>
+                  <span
+                    onClick={() => {
+                      setToggleLogin(true);
+                      setEmail("");
+                      setPassword("");
+                    }}
+                  >
                     &nbsp; Register
                   </span>
                 </small>
               )}
             </div>
           </div>
+          
         </div>
+        {password.length > 0 && (
+              <div className="validation-wrapper">
+                <div>
+                  <small
+                    className={`mx-2 ${
+                      passwordLength ? "font-success" : "font-danger"
+                    }`}
+                  >
+                    <BsCheckCircle
+                      className={` ${
+                        passwordLength ? "font-success" : "font-danger"
+                      }`}
+                    />
+                    At least 8 characters
+                  </small>
+                  <small
+                    className={`mx-2 ${
+                      hasSpecialChar ? "font-success" : "font-danger"
+                    }`}
+                  >
+                    <BsCheckCircle
+                      className={` ${
+                        hasSpecialChar ? "font-success" : "font-danger"
+                      }`}
+                    />
+                    Has special characters
+                  </small>
+                </div>
+                <div>
+                  <small
+                    className={`mx-2 ${
+                      hasUppercase ? "font-success" : "font-danger"
+                    }`}
+                  >
+                    <BsCheckCircle
+                      className={` ${
+                        hasUppercase ? "font-success" : "font-danger"
+                      }`}
+                    />
+                    Has uppercase letter
+                  </small>
+                  <small
+                    className={`mx-2 ${
+                      hasNumber ? "font-success" : "font-danger"
+                    }`}
+                  >
+                    <BsCheckCircle
+                      className={` ${
+                        hasNumber ? "font-success" : "font-danger"
+                      }`}
+                    />
+                    Has number characters
+                  </small>
+                </div>
+              </div>
+            )}
       </div>
     </>
   );
